e65579a2d209e615ab091ae982ff569c1c742d38
Fatima-zah <92028050+Fatima-zah@users.noreply.github.com>
2022-06-20 15:18:05 +0100

    Add files via upload

---
+++ intershipping/first.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="first.css">
+    <title>Document</title>
+</head>
+<body>
+
+<section>
+
+
+<div style=" background-image: linear-gradient(white, #cbcde1);  padding-left:2.5%;  font-family: monospace;
+    font-size: 50px; ; height :50vh;">
+ <h1 style="font-size:50px; padding-top: 10px;color: #5F6579;">OBTENEZ UN DEVIS D'EXPÉDITION INTERNATIONALE GRATUIT EN LIGNE</h1> 
+         <h4 style="width: 845px; font-size: 26px;margin-top: 25px;color: white;">Petits ou grand colis, palettes, nous pouvons vous proposer un devis de vos envois internationaux instantanément. Veuillez clicker sur calculer et saisir ci-dessous les informations relatives à votre envoi pour obtenir votre devis.</h4>
+         
+        </div>
+    <div class="drapos" >
+        
+        <div class="hh">
+            
+        <div class="position">
+        <p>De</p>
+        <img style="width: 80px;height: 60px;;" src="maro.png">
+        <p>Vers</p>
+        <img style="width: 80px;height: 60px;;" src="spaiin.png"> 
+        </div>
+        <div style="text-align:center;">
+        <a href="log-in.php"> <button id="sbm">Calculer</button></a></div>
+</div>
+
+    <div> 
+        <img src="BATT.jpg" alt="" srcset="" width="100%" style="height: 49.3vh;">
+    </div>
+
+    </section>
+    
+
+<!-- <body class="styly"> -->
+    <!-- <section>
+
+        position: absolute;
+    background: rgba(255, 255, 255, 0.59);
+    display: grid;
+    grid-template-columns: 1fr auto 1fr auto;
+    width: 30%;
+    /* grid-template-columns: 1fr 1fr 1fr auto; */
+    gap: 52px;
+    right: 53px;
+    top: 148px;
+
+    
+    <div style="color: white;"> 
+        <h1 style="font-size:50px; ">OBTENEZ UN DEVIS D'EXPÉDITION INTERNATIONALE GRATUIT EN LIGNE</h1>
+        <h4 style="width: 500px; font-size: 26px;">Petits ou grand colis, palettes, nous pouvons vous proposer un devis de vos envois internationaux instantanément. Veuillez saisir ci-dessous, les informations relatives à votre envoi pour obtenir votre devis.</h4>
+    </div>
+    <div class="drapos" >
+        <div class="position">
+        <p>De</p>
+        <img style="width: 80px;height: 70px;;" src="maro.png">
+        <p>Vers</p>
+        <img style="width: 80px;height: 70px;;" src="spaiin.png">
+        <button id="sbm">Submit</button>
+
+    
+            </div>
+
+        </div>
+        <img  src="shipping-services-1.jpg" class="bateau">
+
+        </section> -->
+</body>
+</html>
